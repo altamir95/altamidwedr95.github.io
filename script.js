81ec9f5de9497e9874eb483a09d51597640c32f7
altamir95 <altamir95@yandex.ru>
2020-11-11 17:21:52 +0300

    11.11.2020-17.21

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-// var elem = document.querySelector('.draggable');
-// var draggie = new Draggabilly( elem, {
-//   // options...
-// });
+var div_new = document.getElementsByClassName('droppable')[0];
+var rect = div_new.getBoundingClientRect();
+console.log(rect.top, rect.right, rect.bottom, rect.left);
 
-// // or pass in selector string as first argument
-// var draggie = new Draggabilly( '.draggable', {
-//   // options...
-// });
+var draggie = new Draggabilly('.draggable');
+draggie.on('pointerUp', function (event, pointer) {
 
-// if you have multiple .draggable elements
-// get all draggie elements
-var draggableElems = document.querySelectorAll('.draggable');
-// array of Draggabillies
-var draggies = []
-// init Draggabillies
-for (var i = 0; i < draggableElems.length; i++) {
-    var draggableElem = draggableElems[i];
-    var draggie = new Draggabilly(draggableElem, {
-        // options...
-    });
-    draggies.push(draggie);
-}
-draggie.on('pointerUp', function () {
-    var droppableElem = document.querySelectorAll('.droppable');
-    var rect = droppableElem[0].getBoundingClientRect();
-    console.log(rect.top, this.position.y)
-    if (rect.top > this.position.y) {
-        droppableElem[0].classList.remove('bg-success')
-        // document.getElementById('ff').classList.remove('')
+    if (pointer.pageX > rect.left && pointer.pageX < rect.right&&pointer.pageY>rect.top&&pointer.pageY<rect.bottom) {
+        console.log(pointer.pageX, pointer.pageY);
     }
 });
