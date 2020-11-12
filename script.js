87b2685b1774343066a54e080e3deb8f6acbf533
altamir95 <altamir95@yandex.ru>
2020-11-12 23:52:38 +0300

    12.11.2020-23.52

--- script.js
+++ script.js
@@ -1,59 +1,62 @@
 var array_arab = ['جَمِيلٌ', 'جَدِيدٌ', 'قَدِيم', 'كَبِيرٌ', 'صَغِيرٌ', 'يَسْكُنُ', 'نَشِيطٌ'];
 var array_rus = ['активный', 'жимет', 'маленький', 'большой', 'старый', 'новый', 'красивый'];
 
-for (var i = 0; i < array_arab.length; i++) {
-    create_Element({
-        tagName: 'div',
-        internals: array_arab[i],
-        class: ['btn', 'btn-light', 'btn-lg', 'm-1', 'draggable'],
-        appendPlace: document.body
-    });
-    create_Element({
-        tagName: 'div',
-        internals: array_rus[i],
-        class: ['btn', 'btn-light', 'btn-lg', 'm-1', 'draggable'],
-        appendPlace: document.body
-    });
-}
+// for (var i = 0; i < array_arab.length; i++) {
+//     create_Element({
+//         tagName: 'div',
+//         internals: array_arab[i],
+//         class: ['btn', 'btn-light', 'btn-lg', 'm-1', 'draggable'],
+//         appendPlace: document.body
+//     });
+//     create_Element({
+//         tagName: 'div',
+//         internals: array_rus[i],
+//         class: ['btn', 'btn-light', 'btn-lg', 'm-1', 'draggable'],
+//         appendPlace: document.body
+//     });
+// }
 
 
 var draggableElems = document.querySelectorAll('.draggable');
 // array of Draggabillies
 var draggies = []
+var draggie;
 // init Draggabillies
 for (var i = 0; i < draggableElems.length; i++) {
+    var my_index = i;
     var draggableElem = draggableElems[i];
-    var draggie = new Draggabilly(draggableElem, {
-        // options...
+     draggie = new Draggabilly(draggableElem, {
+        containment: '#transfer-correctly'
     });
-    draggie.on('pointerUp', function (event, pointer) {
-        var div_new = document.getElementsByClassName('droppable')[0];
-        var rect = div_new.getBoundingClientRect();
-        console.log(rect.top, rect.right, rect.bottom, rect.left);
-
-        if (pointer.pageX > rect.left && pointer.pageX < rect.right && pointer.pageY > rect.top && pointer.pageY < rect.bottom) {
-            console.log(pointer.pageX, pointer.pageY);
-            var div_new_new = event.target.cloneNode(true);
-            div_new_new.removeAttribute('style');
-            div_new_new.classList.remove('m-5');
-            div_new.append(div_new_new);
-
-            event.target.remove();
-            //    .classList.remove('btn-light'); 
-        }
-    });
+    draggie.rect = draggableElem.getBoundingClientRect();
     draggies.push(draggie);
-}
-document.getElementById('bts').onclick = function(){
-    var itogarab = document.getElementById('arab');
-    var allitogarab = itogarab.getElementsByClassName('draggable');
-    for(var i = 0;i < allitogarab.length;i++){
-        if(array_rus.indexOf( allitogarab[i].innerHTML ) != -1){
-            document.getElementById('arab').classList.remove('bg-success');
-            document.getElementById('arab').classList.add('btn-danger');
+    draggie.on('pointerUp', function (event, pointer) {
+        var div_new = document.getElementsByClassName('droppable');
+        for (var i = 0; i < div_new.length; i++) {
+            var rect = div_new[i].getBoundingClientRect();
+            console.log(rect.top, rect.right, rect.bottom, rect.left);
+            if (pointer.pageX > rect.left && pointer.pageX < rect.right && pointer.pageY > rect.top && pointer.pageY < rect.bottom) {
+                console.log(pointer.pageX, pointer.pageY);
+                var div_new_new = event.target.cloneNode(true);
+                div_new_new.removeAttribute('style');
+                div_new[i].append(div_new_new);
+                event.target.remove();
+            } else {
+                event.target.removeAttribute('style');
+            }
         }
-    }
-};
+    });
+}
+// document.getElementById('bts').onclick = function(){
+//     var itogarab = document.getElementById('arab');
+//     var allitogarab = itogarab.getElementsByClassName('draggable');
+//     for(var i = 0;i < allitogarab.length;i++){
+//         if(array_rus.indexOf( allitogarab[i].innerHTML ) != -1){
+//             document.getElementById('arab').classList.remove('bg-success');
+//             document.getElementById('arab').classList.add('btn-danger');
+//         }
+//     }
+// };
 
 
 
